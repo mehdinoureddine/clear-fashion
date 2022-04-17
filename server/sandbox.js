1318b837a35faface6af09eec86fbcbf70765c9d
mehdinoureddine <96992200+mehdinoureddine@users.noreply.github.com>
2022-04-17 16:29:07 +0200

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,21 +1,67 @@
-/* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
-
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+const dedicatedbrand = require('./sites/dedicatedbrand');
+const loom = require('./sites/loom');
+const db = require('./db');
+MONGODB_URI = "mongodb+srv://clearfashion:<1234>@cluster0.wazsb.mongodb.net/Clear-fashion?retryWrites=true&w=majority"
+async function sandbox () {
   try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+    let products = [];
+    let pages = [
+      'https://www.dedicatedbrand.com/en/men/basics',
+      'https://www.dedicatedbrand.com/en/men/sale'
+    ];
 
-    const products = await dedicatedbrand.scrape(eshop);
+    console.log(' browsing ${pages.length} pages ...`);
 
-    console.log(products);
-    console.log('done');
-    process.exit(0);
+    for (let page of pages) {
+      console.log(` scraping ${page}`);
+      let results = await dedicatedbrand.scrape(page);
+      console.log(` ${results.length} products found !`);
+      products.push(results);
+    }
+
+    pages = [
+      'https://www.loom.fr/collections/hauts',
+      'https://www.loom.fr/collections/bas'
+    ];
+
+    console.log('\n');
+
+    console.log(` browsing ${pages.length} pages using the Promise.all function`);
+    const promises = pages.map(page => loom.scrape(page));
+    const results = await Promise.all(promises);
+    console.log(` ${results.length} results of promises found`);
+    console.log(` ${results.flat().length} products found`);
+    console.log(results);
+    console.log(results.flat());
+    products.push(results.flat());
+    products = products.flat();
+
+    console.log('\n');
+
+    console.log(` ${products.length} total of products found`);
+
+    console.log('\n');
+
+    const { MongoClient } = require('mongodb');
+    const client = new MongoClient(MONGODB_URI);
+    
+    await client.connect();
+    const database = client.db("Clear-fashion");
+    const prods = database.collection("product");
+    const result = await prods.insertMany(products);;
+    console.log(` ${result.insertedCount} inserted products`);
+
+    console.log('\n');
+
+    console.log(' Find "Loom" products only');
+    const loomOnly = await prods.find({'brand': 'loom'});
+    console.log(` ${loomOnly.length} products found for the brand "Loom" `);
+    console.log(loomOnly);
+
+    await client.close();
   } catch (e) {
     console.error(e);
-    process.exit(1);
   }
 }
 
-const [,, eshop] = process.argv;
-
-sandbox(eshop);
+sandbox();
